5a768a23d52c1bdb945079d9be48e6dfe8a62c89
Jorge Osvaldo Varela <94763025+osvaldovarela@users.noreply.github.com>
2024-06-22 02:00:03 -0300

    update

---
+++ controllers/productos.controller.js
@@ -0,0 +1,106 @@
+const db = require("../db/db");
+
+
+//Esto es el CRUD. Se puede probar con Postman.
+
+//esto es para ver lo q hay en la base de datos/método GET
+
+const index = (req, res) => {
+  const sql = "SELECT * FROM productos";
+
+  db.query(sql, (error, rows) => {
+    if (error) {
+      return res.status(500).json({ error: "Intente mas tarde" });
+    }
+
+    res.json(rows);
+  });
+};
+
+//esto es para ver un elemento de la base de datos/método GET
+
+const show = (req, res) => {
+  const { id } = req.params;
+
+  const sql = "SELECT * FROM productos WHERE id = ?";
+  db.query(sql, [id], (error, rows) => {
+    if (error) {
+      return res.status(500).json({ error: "Intente mas tarde" });
+    }
+
+    if (rows.length == 0) {
+      return res.status(404).json({ error: "No existe el producto" });
+    }
+
+    res.json(rows[0]);
+  });
+};
+
+//esto es para crear un elemento de la base de datos/método POST
+
+const store = (req, res) => {
+  const { nombre, stock, precio } = req.body;
+
+  const sql = "INSERT INTO productos (nombre, precio, stock) VALUES (?, ?, ?)";
+  db.query(sql, [nombre, precio, stock], (error, result) => {
+    if (error) {
+      return res.status(500).json({ error: "Intente mas tarde" });
+    }
+
+    const producto = { ...req.body, id: result.insertId };
+
+    res.json(producto);
+  });
+};
+
+
+//esto es para modificar un elemento de la base de datos/método PUT
+
+const update = (req, res) => {
+  const { id } = req.params;
+  const { nombre, stock, precio } = req.body;
+
+  const sql =
+    "UPDATE productos SET nombre = ?, stock = ?, precio = ? WHERE id = ?";
+  db.query(sql, [nombre, stock, precio, id], (error, result) => {
+    if (error) {
+      return res.status(500).json({ error: "Intente mas tarde" });
+    }
+
+    if (result.affectedRows == 0) {
+      return res.status(404).json({ error: "No existe el producto" });
+    }
+
+    const producto = { ...req.body, ...req.params };
+
+    res.json(producto);
+  });
+};
+
+
+//esto es para eliminar un elemento de la base de datos/método DELETE
+
+const destroy = (req, res) => {
+  const { id } = req.params;
+
+  const sql = "DELETE FROM productos WHERE id = ?";
+  db.query(sql, [id], (error, result) => {
+    if (error) {
+      return res.status(500).json({ error: "Intente mas tarde" });
+    }
+
+    if (result.affectedRows == 0) {
+      return res.status(404).json({ error: "No existe el producto" });
+    }
+
+    res.json({ mensaje: `Producto ${id} borrado` });
+  });
+};
+
+module.exports = {
+  index,
+  show,
+  store,
+  update,
+  destroy,
+};
